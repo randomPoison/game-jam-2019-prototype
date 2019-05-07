911148a96b0461a3d36224842749499e97fa7c8a
David LeGare <dlegare.1001@gmail.com>
2019-05-07 13:14:51 -0500

    Add utility for generating component IDs

---
+++ workers/unity/Assets/Editor/EditorUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class EditorUtils
+{
+    [MenuItem("SpatialOS/Tools/Generate Component ID")]
+    public static int GenerateComponentId()
+    {
+        while (true)
+        {
+            var candidate = Random.Range(100, 536_870_910);
+            if (candidate < 190_000 || candidate > 199_999)
+            {
+                Debug.Log("Component ID: " + candidate);
+                return candidate;
+            }
+        }
+    }
+}
