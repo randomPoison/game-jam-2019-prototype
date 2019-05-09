b599b80a49ff9cd0ad8afea7495568af106871d5
David LeGare <dlegare.1001@gmail.com>
2019-05-08 22:20:17 -0500

    Add Drone component and entity template

---
+++ workers/unity/Assets/Scripts/EntityTemplates.cs
@@ -0,0 +1,35 @@
+﻿using BetaApartUranus;
+using Improbable;
+using Improbable.Gdk.Core;
+using UnityEngine;
+
+namespace BlankProject
+{
+    public static class EntityTemplates
+    {
+        public static EntityTemplate Drone(GridCoordinate position, uint playerId)
+        {
+            // Create a HealthPickup component snapshot which is initially active and grants "heathValue" on pickup.
+            var droneComponent = new Drone.Snapshot(playerId);
+            var gridPositionComponent = new GridPosition.Snapshot(position);
+            var worldPosition = new Coordinates()
+            {
+                X = Mathf.Sqrt(3f) * position.Col + Mathf.Sqrt(3f) / 2f * position.Row,
+                Y = 3f / 2f * position.Row,
+                Z = 0f,
+            };
+
+            var entityTemplate = new EntityTemplate();
+            entityTemplate.AddComponent(new Position.Snapshot(worldPosition), WorkerUtils.UnityGameLogic);
+            entityTemplate.AddComponent(new Metadata.Snapshot("HealthPickup"), WorkerUtils.UnityGameLogic);
+            entityTemplate.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
+            entityTemplate.AddComponent(droneComponent, WorkerUtils.UnityGameLogic);
+            entityTemplate.AddComponent(gridPositionComponent, WorkerUtils.UnityGameLogic);
+            entityTemplate.SetReadAccess(WorkerUtils.UnityGameLogic, WorkerUtils.UnityClient);
+            entityTemplate.SetComponentWriteAccess(EntityAcl.ComponentId, WorkerUtils.UnityGameLogic);
+
+            return entityTemplate;
+        }
+    }
+}
+
---
+++ workers/unity/Assets/Scripts/Workers/WorkerUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace BlankProject
+{
+    public static class WorkerUtils
+    {
+        public const string UnityClient = "UnityClient";
+        public const string AndroidClient = "AndroidClient";
+        public const string iOSClient = "iOSClient";
+
+        public const string UnityGameLogic = "UnityGameLogic";
+        public const string SimulatedPlayerCoordinator = "SimulatedPlayerCoordinator";
+    }
+}
