6e67775c5863bd9eacbcdb52ade1a178519f26e2
David LeGare <dlegare.1001@gmail.com>
2019-05-31 12:42:33 -0500

    Spawn drone entities at random positions

--- workers/unity/Assets/Scripts/Game Logic/SpawnControllerCommandHandler.cs
+++ workers/unity/Assets/Scripts/Game Logic/SpawnControllerCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using Improbable.Gdk.Subscriptions;
+﻿using Improbable.Gdk.Core;
+using Improbable.Gdk.Core.Commands;
+using Improbable.Gdk.Subscriptions;
 using UnityEngine;
 
 namespace BetaApartUranus
@@ -8,6 +10,9 @@ namespace BetaApartUranus
         [Require]
         private SpawnControllerCommandReceiver _commandReceiver = null;
 
+        [Require]
+        private WorldCommandSender _worldSender = null;
+
         private void OnEnable()
         {
             _commandReceiver.OnSpawnDroneRequestReceived += OnSpawnDroneRequest;
@@ -15,8 +20,22 @@ namespace BetaApartUranus
 
         private void OnSpawnDroneRequest(SpawnController.SpawnDrone.ReceivedRequest request)
         {
-            Debug.Log("Handling spawn drone request!");
-            _commandReceiver.SendSpawnDroneResponse(request.RequestId, new SpawnDroneResponse());
+            Debug.Log($"Handling spawn drone request {request.EntityId} from {request.CallerWorkerId}");
+
+            // Generate a random starting point for the drone.
+            var position = new GridCoordinate(
+                Random.Range(-20, 20),
+                Random.Range(-20, 20));
+
+            // Create an entity for the drone, and once it's been made, respond to the player.
+            //
+            // TODO: Can we do this with async/await, rather than callbacks?
+            _worldSender.SendCreateEntityCommand(
+                new WorldCommands.CreateEntity.Request(EntityTemplates.Drone(position, request.CallerWorkerId)),
+                response =>
+                {
+                    _commandReceiver.SendSpawnDroneResponse(request.RequestId, new SpawnDroneResponse());
+                });
         }
     }
 }
