2a17aa33cf91f449a0eb9f78fe1584adb66f67b4
David LeGare <dlegare.1001@gmail.com>
2019-06-12 13:57:15 -0500

    Display owned drones with a different material

--- workers/unity/Assets/Scripts/Game Logic/ClientDrone.cs
+++ workers/unity/Assets/Scripts/Game Logic/ClientDrone.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Improbable.Gdk.Subscriptions;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace BetaApartUranus
@@ -8,12 +9,38 @@ namespace BetaApartUranus
     /// </summary>
     public class ClientDrone : MonoBehaviour, IPointerDownHandler
     {
+        [SerializeField]
+        [Tooltip("The material to use when the drone is owned by the current player.")]
+        private Material _playerMaterial = null;
+
+        [SerializeField]
+        [Tooltip("The material to use when the drone is owned by another player.")]
+        private Material _otherMaterial = null;
+
+        [Require]
+        private DroneReader _droneReader = null;
+
+        private MeshRenderer _display;
+        private UnityClientConnector _connector;
         private ClientController _clientController;
 
         #region Unity Lifecycle Methods
         private void Awake()
         {
+            _display = GetComponentInChildren<MeshRenderer>();
+            _connector = FindObjectOfType<UnityClientConnector>();
             _clientController = FindObjectOfType<ClientController>();
+        }
+
+        private void Start()
+        {
+            // TODO: Is there a better way to check if the drone is owned by the
+            // current worker? It seems odd to have a reference to the connector
+            // object just to fetch the worker ID.
+            if (_droneReader.Data.Owner == _connector.Worker.WorkerId)
+            {
+                _display.sharedMaterial = _playerMaterial;
+            }
         }
         #endregion
 
