24ca1a317189b54e4246614906cd6edfa54b3a78
David LeGare <dlegare.1001@gmail.com>
2019-06-14 14:49:44 -0500

    Reject drone commands from non-owning players

--- workers/unity/Assets/Scripts/Game Logic/DroneCommandHandler.cs
+++ workers/unity/Assets/Scripts/Game Logic/DroneCommandHandler.cs
@@ -8,13 +8,24 @@ namespace BetaApartUranus
     public class DroneCommandHandler : MonoBehaviour
     {
         [Require]
-        private DroneCommandReceiver _commandReceiver = null;
+        private DroneReader _droneReader = null;
 
-        //[Require]
-        //private WorldCommandSender _worldSender = null;
+        [Require]
+        private DroneCommandReceiver _commandReceiver = null;
 
         private void OnAddCommandRequest(Drone.AddCommand.ReceivedRequest request)
         {
+            // Reject attempts to add commands to a drone not owned by the player.
+            if (_droneReader.Data.Owner != request.CallerWorkerId)
+            {
+                Debug.Log($"Rejecting add command request {request.EntityId} from {request.CallerWorkerId}");
+
+                _commandReceiver.SendAddCommandFailure(
+                    request.RequestId,
+                    "Cannot add command to non-owned drone");
+                return;
+            }
+
             Debug.Log($"Handling add command request {request.EntityId} from {request.CallerWorkerId}");
             _commandReceiver.SendAddCommandResponse(request.RequestId, new AddCommandResponse());
         }
