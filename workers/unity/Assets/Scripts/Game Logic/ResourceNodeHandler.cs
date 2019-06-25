dc1525d0ccfa38ea7b937203a50b1e6322f270a1
ricky <roberson3d@gmail.com>
2019-06-25 16:42:56 -0500

    destroy resource node once it's empty

---
+++ workers/unity/Assets/Scripts/Game Logic/ResourceNodeHandler.cs
@@ -0,0 +1,30 @@
+﻿using Improbable.Gdk.Core;
+using Improbable.Gdk.Core.Commands;
+using Improbable.Gdk.Subscriptions;
+using UnityEngine;
+
+namespace BetaApartUranus
+{
+    public class ResourceNodeHandler : MonoBehaviour
+    {
+        [Require]
+        private EntityId _entityId = new EntityId();
+
+        [Require]
+        private ResourceNodeReader _nodeReader = null;
+
+        [Require]
+        private WorldCommandSender _worldSender = null;
+
+        private void LateUpdate ()
+        {
+            if (_nodeReader.Data.Quantity <= 0)
+            {
+                // Delete the resource node if there isn't any resources left
+                _worldSender.SendDeleteEntityCommand(
+                    new WorldCommands.DeleteEntity.Request(_entityId)
+                );
+            }
+        }
+    }
+}
