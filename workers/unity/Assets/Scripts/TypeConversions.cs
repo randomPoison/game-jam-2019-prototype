809aa02137579bc4da617662872ac59100121856
David LeGare <dlegare.1001@gmail.com>
2019-06-25 15:18:13 -0500

    Fill default snapshot with resource nodes

---
+++ workers/unity/Assets/Scripts/TypeConversions.cs
@@ -0,0 +1,12 @@
+﻿using HexTools;
+
+namespace BetaApartUranus
+{
+    public static class TypeConversions
+    {
+        public static GridCoordinate ToGridCoordinate(this AxialCoordinate axial)
+        {
+            return new GridCoordinate(axial.Q, axial.R);
+        }
+    }
+}
